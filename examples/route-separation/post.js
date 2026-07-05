8fd4ffda5db23040bd909b812d3140531fec6316
sadia-54 <sadiajessia@gmail.com>
2026-07-05 14:41:26 +0600

    ANOTHER titlw added

--- examples/route-separation/post.js
+++ examples/route-separation/post.js
@@ -6,7 +6,8 @@ var posts = [
   { title: 'Foo', body: 'some foo bar' },
   { title: 'Foo bar', body: 'more foo bar' },
   { title: 'Foo bar baz', body: 'more foo bar baz' },
-  { title: 'Foo bar baz qux', body: 'more foo bar baz qux' }
+  { title: 'Foo bar baz qux', body: 'more foo bar baz qux' },
+  { title: 'Foo bar baz qux quux', body: 'more foo bar baz qux quux' }
 ];
 
 exports.list = function(req, res){
