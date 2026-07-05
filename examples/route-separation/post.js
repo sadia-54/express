c14c8226658d68c97d03fbd241cf63b1004acb54
sadia-54 <sadiajessia@gmail.com>
2026-07-05 14:40:20 +0600

    another post title added

--- examples/route-separation/post.js
+++ examples/route-separation/post.js
@@ -5,9 +5,11 @@
 var posts = [
   { title: 'Foo', body: 'some foo bar' },
   { title: 'Foo bar', body: 'more foo bar' },
-  { title: 'Foo bar baz', body: 'more foo bar baz' }
+  { title: 'Foo bar baz', body: 'more foo bar baz' },
+  { title: 'Foo bar baz qux', body: 'more foo bar baz qux' }
 ];
 
 exports.list = function(req, res){
   res.render('posts', { title: 'Posts', posts: posts });
 };
+
