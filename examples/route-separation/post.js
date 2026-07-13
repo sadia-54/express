8642007921a136820f0b6df0f0c6542bb0d3091b
sadia-54 <sadiajessia@gmail.com>
2026-07-13 22:56:24 +0600

    another fake post added

--- examples/route-separation/post.js
+++ examples/route-separation/post.js
@@ -8,7 +8,8 @@ var posts = [
   { title: 'Foo bar baz', body: 'more foo bar baz' },
   { title: 'Foo bar baz qux', body: 'more foo bar baz qux' },
   { title: 'Foo bar baz qux quux', body: 'more foo bar baz qux quux' },
-   { title: 'Fooo bar baz qux quux', body: 'more foo bar baz qux quux' }
+   { title: 'Fooo bar baz qux quux', body: 'more foo bar baz qux quux' },
+    { title: 'Fooo bar baz qux quux', body: 'more foo bar baz qux quux' }
 ];
 
 exports.list = function(req, res){
